92a8843601045014b04eb72d79723f16ff66350e
Roman Sotnikov <axeman.ofic@gmail.com>
2025-08-11 17:45:06 +0300

    feat: update terminal size

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -11,21 +11,23 @@ map("i", "jk", "<ESC>")
 
 -- TERMINAL
 local term_v = function()
-  require("nvchad.term").toggle { pos = "vsp" }
-end
-
-local term_h = function()
-  require("nvchad.term").toggle { pos = "sp" }
+  require("nvchad.term").toggle { pos = "vsp", size = 0.6 }
 end
 
 local term_f = function()
-  require("nvchad.term").toggle { pos = "float" }
+  require("nvchad.term").toggle {
+    pos = "float",
+    float_opts = {
+      height = 0.6,
+      row = 0.1,
+    },
+  }
 end
 
 map("n", "<C-]>", term_v, { desc = "Open vertical terminal" })
-map("n", "<C-\\>", term_h, { desc = "Open horizontal terminal" })
+map("n", "<C-\\>", term_f, { desc = "Open float terminal" })
 map("n", "<C-f>", term_f, { desc = "Open float terminal" })
 --
 map("t", "<C-]>", term_v, { desc = "Closed vertical terminal" })
-map("t", "<C-\\>", term_h, { desc = "Closed horizontal terminal" })
+map("t", "<C-\\>", term_f, { desc = "Closed float terminal" })
 map("t", "<C-f>", term_f, { desc = "Closed float terminal" })
