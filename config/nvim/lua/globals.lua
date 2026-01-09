f9a93f50477554ee58c377040656e9b4bc65ba3d
Roman Sotnikov <axeman.ofic@gmail.com>
2026-01-09 18:32:13 +0300

    feat: add snippets

---
+++ config/nvim/lua/globals.lua
@@ -0,0 +1,6 @@
+-- https://nvchad.com/docs/config/snippets/
+-- https://github.com/L3MON4D3/LuaSnip/blob/master/DOC.md#loaders
+
+local g = vim.g
+
+g.vscode_snippets_path = vim.fn.stdpath "config" .. "snippets"
